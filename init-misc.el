9cc1361533dcc272d939d46f5b7deb2629cef60e
feidouqwer <feidouqwer@163.com>
2015-10-27 15:15:23 +0800

    feature: add f4 to shell-command and f5 to compile

--- init-misc.el
+++ init-misc.el
@@ -7,7 +7,17 @@
 (setq google-translate-listen-url
       "http://translate.google.cn/translate_tts")
 
-(evil-leader/set-key "xgy" 'youdao-dictionary-search-at-point+)
-(evil-leader/set-key "xgY" 'youdao-dictionary-search-at-point)
+(defun qwer/find-user-bashrc-file ()
+  "Edit the ~/.bashrc, in the current window."
+  (interactive)
+  (find-file-existing "~/.bashrc"))
+(evil-leader/set-key "feb" 'qwer/find-user-bashrc-file)
+
+(global-set-key (kbd "<f5>") 'compile)
+(global-set-key (kbd "<f4>") 'shell-command)
+
+(if (configuration-layer/package-usedp 'youdao-dictionary)
+    (progn (evil-leader/set-key "xgy" 'youdao-dictionary-search-at-point+)
+           (evil-leader/set-key "xgY" 'youdao-dictionary-search-at-point)))
 
 (provide 'init-misc)
